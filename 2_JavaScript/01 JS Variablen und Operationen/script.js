f9fe5bdf5b5fd92767b591581bb9d9c6a2926490
David Bischof <david.bischof07@gmail.com>
2024-10-19 19:24:39 +0200

    01 JS Variablen und Operationen done

---
+++ 2_JavaScript/01 JS Variablen und Operationen/script.js
@@ -0,0 +1,74 @@
+console.log("Hallo");
+console.log("Tolle Sache");
+
+//Variablen
+let a = 7;
+let b = 8;
+let c = a + b;
+
+console.log("A hat den Wert " + a);
+
+a = 10;
+console.log("A hat den Wert " + a);
+
+console.log("a + b = " + c);
+
+
+//Rechnungsarten
+// *, /, +, -
+
+c = b - a
+console.log("b - a = " + c);
+
+
+
+
+
+//Berechne folgendes und gib das Ergebnis über console.log aus
+
+let result = a + b + c;
+console.log("Das Ergebnis ist " + result);
+//a + b + c
+
+result = (a + b) / c;
+console.log("Das Ergebnis ist " + result);
+//(a + b) / c
+
+result = c - a + b;
+console.log("Das Ergebnis ist " + result);
+//c - a + b 
+
+result = a + b + c * 3;
+console.log("Das Ergebnis ist " + result);
+//a + b + c * 3
+
+result = (a + b + c) / 4;
+console.log("Das Ergebnis ist " + result);
+//(a + b + c) / 4
+
+
+
+//Ändere die Variablen auf folgende Werte
+
+
+//a = 3
+console.log("a vor Änderung: " + a);
+a = 3;
+console.log("a nach Änderung: " + a)
+
+
+//b = a + 3
+console.log("b vor Änderung: " + b);
+b = a + 3;
+console.log("b nach Änderung: " + b)
+
+
+//c = c - 3
+console.log("c vor Änderung: " + c);
+c = c - 3;
+console.log("c nach Änderung: " + c)
+
+
+
+
+//Gib die neuen Werte der Variablen aus
