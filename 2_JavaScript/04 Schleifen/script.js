3e481b83ca429c2ad41fa0a99ea68d645f2ecfa9
David Bischof <david.bischof07@gmail.com>
2024-10-19 20:47:29 +0200

    04 Schleifen done

---
+++ 2_JavaScript/04 Schleifen/script.js
@@ -0,0 +1,43 @@
+    console.log("Test");
+
+    let a = 7;
+    let b = 4.3;
+    let name = "David"; //Text
+
+    console.log(name + " " + b + a);
+
+    //Schleifen
+    for(let count = 10; count < 100; count++)
+    {
+        console.log(count + name);
+    }
+
+    for(let count = 10; count > 0; count--)
+    {
+        console.log(count + name);
+    }
+        
+    console.log("Fertig");
+
+
+    for(let count = 7; count < 45; count++)
+    {
+        console.log(count);
+    }
+
+    for(let count = 9; count < 99; count++)
+    {
+        console.log(count);
+    }
+
+    for(let count = 10; count > 0; count--)
+    {
+        console.log(count);
+    }
+
+    for(let count = 1234; count > 1207; count--)
+    {
+        console.log(count);
+    }
+
+    console.log("Fertig2")
