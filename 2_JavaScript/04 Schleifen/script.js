1310118c8e5ba019fd1ad2ed2de44ce6384a61c4
David Bischof <david.bischof07@gmail.com>
2024-10-19 21:20:27 +0200

    05 If-Bedingung done

--- 2_JavaScript/04 Schleifen/script.js
+++ 2_JavaScript/04 Schleifen/script.js
@@ -1,43 +1,66 @@
-    console.log("Test");
+console.log("Test");
 
-    let a = 7;
-    let b = 4.3;
-    let name = "David"; //Text
+let a = 7;
+let b = 4.3;
+let name = "David"; //Text
 
-    console.log(name + " " + b + a);
+console.log(name + " " + b + a);
 
-    //Schleifen
-    for(let count = 10; count < 100; count++)
-    {
-        console.log(count + name);
-    }
+//Schleifen
+for(let count = 10; count < 100; count++)
+{
+    console.log(count + name);
+}
 
-    for(let count = 10; count > 0; count--)
-    {
-        console.log(count + name);
-    }
-        
-    console.log("Fertig");
+for(let count = 10; count > 0; count--)
+{
+    console.log(count + name);
+}
+    
+console.log("Fertig");
 
 
-    for(let count = 7; count < 45; count++)
-    {
-        console.log(count);
-    }
+for(let count = 7; count < 45; count++)
+{
+    console.log(count);
+}
 
-    for(let count = 9; count < 99; count++)
-    {
-        console.log(count);
-    }
+for(let count = 9; count < 99; count++)
+{
+    console.log(count);
+}
 
-    for(let count = 10; count > 0; count--)
-    {
-        console.log(count);
-    }
+for(let count = 10; count > 0; count--)
+{
+    console.log(count);
+}
 
-    for(let count = 1234; count > 1207; count--)
-    {
-        console.log(count);
-    }
+for(let count = 1234; count > 1207; count--)
+{
+    console.log(count);
+}
 
-    console.log("Fertig2")
+console.log("Fertig2")
+
+
+// Gib Deinen Namen genau 8 mal aus
+
+
+for(let count = 0; count < 8; count++)
+{
+    console.log("David");
+}
+
+
+// Zähle von 0 auf 10, dann gib Deinen Namen aus, dann zähle von 10 auf 0
+for(let count = 0; count < 11; count++)
+{
+    console.log(count);
+}
+
+console.log("David");
+
+for(let count = 10; count > -1; count--)
+{
+    console.log(count);
+}
