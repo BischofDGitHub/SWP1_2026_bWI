9d7443bf92e5111f55b1a5db3cfd73636033b7ec
David Bischof <david.bischof07@gmail.com>
2024-10-19 19:44:38 +0200

    02 JS Strings und Zahlen done

---
+++ 2_JavaScript/02 JS Strings und Zahlen/script.js
@@ -0,0 +1,49 @@
+let a = 7;
+let b = 8;
+
+let result = a - b;
+
+console.log(result);
+
+// -, -, *, /
+
+let c = 7.4;
+let d = 7.5;
+
+result = c + d;
+console.log(result);
+
+// Texte STRINGS
+
+let name = "David";
+console.log(name);
+
+
+result = a + b;
+console.log("Das Ergebnis von " + a + " und " + b + " ist " + result);
+
+
+
+
+
+//Aufgabe: Berechne das Ergebnis von 17.4 / 3.9. Gib folgenden Satz aus:
+//The result of 17.4 divided by 3.9 is 4.461538461538462
+
+a = 17.4;
+b = 3.9;
+
+result = a/b;
+console.log("The result of " + a + " divided by " + b + " is " + result);
+console.log("The result of " + a + " divided by " + b + " is " + a/b);
+
+
+
+//Aufgabe: Lege eine Variable firstname und eine Varialbe lastname an. Weise den Variablen Deinen Namen zu. Gib folgendes aus:
+//"Ich heiße VORNAME NACHNAME"
+
+let firstname = "David";
+let lastname = "Bischof";
+let fullname = firstname + " " + lastname;
+
+console.log("Ich heiße " + fullname);
+console.log("Ich heiße " + firstname + " " + lastname);
