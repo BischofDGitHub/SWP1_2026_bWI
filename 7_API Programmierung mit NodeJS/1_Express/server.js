418913b4e6bd506a853097841031920e87954f54
David Bischof <david.bischof07@gmail.com>
2025-01-23 22:41:14 +0100

    1_Express done

---
+++ 7_API Programmierung mit NodeJS/1_Express/server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+ 
+const port = 3000;
+
+const data = [
+    { name: 'Max', age: 27 },
+    { name: 'paul', age: 27 },
+    { name: 'peter', age: 27 },
+    { name: 'matteo', age: 27 }
+]
+
+
+app.get('/people', (req, res) => {
+    res.send(data);
+})
+
+
+app.get('/people/:id', (req, res) => {
+    let id = req.params.id;
+    res.send(data[id]);
+})
+
+
+app.delete('/people/:id', (req, res) => {
+    let id = req.params.id;
+    data.splice(id, 1);
+    res.send("done");
+})
+
+
+app.post("/people", (req, res) => {
+    data.push(req.body);
+    res.send(req.body);
+})
+
+
+app.listen(port, () => {
+    console.log("Server running on port " + port);
+})
